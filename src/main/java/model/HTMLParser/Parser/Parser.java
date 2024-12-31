99896a58088b22981673398bdca09b5469df2ee4
Timur Rakhmatullaev <timurrahmatullaev703@gmail.com>
2024-12-31 08:47:38 +0600

    add Parser

---
+++ src/main/java/model/HTMLParser/Parser/Parser.java
@@ -0,0 +1,27 @@
+package model.HTMLParser.Parser;
+
+import Tokenizer.Tokenizer;
+import Tokenizer.Tokens.CharacterToken;
+import Tokenizer.Tokens.EndOfFileToken;
+import Tokenizer.Tokens.TagToken;
+
+public class Parser {
+    private final Tokenizer tokenizer;
+
+    public Parser(String input) {
+        this.tokenizer = new Tokenizer(this, input);
+        tokenizer.tokenize();
+    }
+
+    public void onCharacterToken(CharacterToken characterToken) {
+        System.out.println(characterToken.getData());
+    }
+
+    public void onOpenTagToken(TagToken tagToken) {
+        System.out.println("<" + tagToken.getTagName() + ">");
+    }
+
+    public void onEndOfFileToken(EndOfFileToken endOfFileToken) {
+        System.out.println("eof");
+    }
+}
