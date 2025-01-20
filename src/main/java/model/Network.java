55206f1fd84105ce7259cf053d4e767bdf879ed3
davletmegahorosh <davletazimov142@gmail.com>
2025-01-20 20:46:03 +0600

    get image

--- src/main/java/model/Network.java
+++ src/main/java/model/Network.java
@@ -1,4 +1,6 @@
 package model;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.net.*;
 import java.io.*;
 import java.util.HashMap;
@@ -8,34 +10,63 @@ public class Network {
     private URL link;
     private Map<String, String> headers = new HashMap<>();
 
-    public String request(String url) throws Exception {
-        link = new URL(url);
-        HttpURLConnection connection = (HttpURLConnection) link.openConnection();
-        connection.setRequestMethod("GET");
-        updateHeaders();
-        if (headers != null) {
-            for (Map.Entry<String, String> entry : headers.entrySet()) {
-                connection.setRequestProperty(entry.getKey(), entry.getValue());
+    public String getPage(String urlString) throws Exception {
+
+        URL url = new URL(urlString);
+        String host = url.getHost();
+        int port = url.getPort() == -1 ? 80 : url.getPort();
+        try (Socket socket = new Socket(host, port);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+
+            headers = getHeaders();
+
+
+            if (headers != null) {
+                for (Map.Entry<String, String> entry : headers.entrySet()) {
+                    out.print(entry.getKey() + ": " + entry.getValue() + "\r\n");
+                }
             }
-        }
+            out.flush();
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        StringBuilder response = new StringBuilder();
-        String inputLine;
+            StringBuilder response = new StringBuilder();
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine).append("\n");
+            }
 
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine).append("\n");
+            System.out.println(response);
+            return response.toString();
         }
-        in.close();
-        return response.toString();
     }
 
-    private Map<String, String> updateHeaders(){
-        headers.clear();
-//        headers.put("Host", "www.youtube.com");
+    public BufferedImage getImage(String urlString) throws Exception {
+        URL url = new URL(urlString);
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+
+        int responseCode = connection.getResponseCode();
+        if (responseCode != HttpURLConnection.HTTP_OK) {
+            throw new IOException("Failed to fetch image. HTTP response code: " + responseCode);
+        }
+
+        try (InputStream inputStream = connection.getInputStream()) {
+            return ImageIO.read(inputStream);
+        }
+    }
+
+
+
+    private Map<String, String> getHeaders() {
+        Map<String, String> headers = new HashMap<>();
         headers.put("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9");
-        headers.put("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36");
         headers.put("Cookie", "VISITOR_INFO1_LIVE=Y-PbsKo7FYg; VISITOR_PRIVACY_METADATA=CgJLRxIEGgAgRg%3D%3D; PREF=f7=4100&f4=4000000&tz=Asia.Bishkek&f5=20000&f6=40000000; YSC=fmZXz5bkpBg; SID=g.a000rwgvoxAAkf6YWuWDFSb-Q9Fi09dIpcm0q3A13RCv-U6IJi1LVR7kmbgvUHyE7hzkTWFCfwACgYKAbkSARYSFQHGX2Miei10zRL3bx9DjyAOmZ_oVxoVAUF8yKo9VdRBqFEWvtrtqF0BhDvy0076; __Secure-1PSID=g.a000rwgvoxAAkf6YWuWDFSb-Q9Fi09dIpcm0q3A13RCv-U6IJi1L2mnfj8wuokPbarjTte8FAAACgYKAXcSARYSFQHGX2MiDNuW5Cw4Ac7tLbaSD6dkhxoVAUF8yKoNaF5H8EgiiwKe5jtoGlIp0076; __Secure-3PSID=g.a000rwgvoxAAkf6YWuWDFSb-Q9Fi09dIpcm0q3A13RCv-U6IJi1L9M4tOR5474gUbFcZ5t1z8gACgYKAVgSARYSFQHGX2MiULXNFtj0HgmgE-EFGALWuxoVAUF8yKqsRf61bYgCvD4hHE_1yCFu0076");
+        headers.put("Content-Type", "application/json");
+        headers.put("User-Agent", "Custom-Client/1.0");
+        headers.put("Authorization", "Bearer YOUR_ACCESS_TOKEN");
+
         return headers;
     }
+
 }
